bc64d4e014d3f217cf69a3eaf10019514ad01701
Thiago Rocha de Aquino <thiagorocha948@uni9.edu.br>
2025-07-24 21:58:37 -0300

    update boolean

--- tipo-booleano.js
+++ tipo-booleano.js
@@ -11,11 +11,14 @@ const aprovado = true;
 rl.question('Digite a nota do estudante: ', (input) => {
   const nota = parseFloat(input);
 
-  if (aprovado) {
+  if (nota >= 7) {
     console.log(`O estudante ${estudante} foi aprovado com a nota ${nota}`);
+    console.log(`O estudante ${estudante} tem o status de aprovação: ${aprovado}`);
   } else {
     console.log(`O estudante ${estudante} não foi aprovado com a nota ${nota}`);
+    console.log(`O estudante ${estudante} tem o status de aprovação: ${!aprovado}`);
   }
-
+ 
   rl.close(); 
 });
+
