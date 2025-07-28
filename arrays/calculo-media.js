865891655edd898109bb9ce994ae9167ffd3f7ed
Thiago Rocha de Aquino <thiagorocha948@uni9.edu.br>
2025-07-28 20:45:08 -0300

    feat(main): usando array para calcular media

---
+++ arrays/calculo-media.js
@@ -0,0 +1,4 @@
+const notas = [10, 6.5, 8, 7.5];
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) /notas.length;//lengeth é o tamanho do array, ou seja, a quantidade de elementos que ele possui. 
+                                                                        //length é uma propriedade do array que retorna o número de elementos contidos nele.
+console.log(`A média é: ${media}`);
