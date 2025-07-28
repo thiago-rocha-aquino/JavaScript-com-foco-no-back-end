caffda9489365343a50798781319cb6457a12c28
Thiago Rocha de Aquino <thiagorocha948@uni9.edu.br>
2025-07-28 18:00:07 -0300

    add operadores-comparacao.js with comparison logic examples

---
+++ operadores-comparacao.js
@@ -0,0 +1,27 @@
+const aprovado = true;
+
+if (aprovado === true){
+   console.log("Aprovado");    
+}
+
+if (aprovado === 1) {
+   console.log("Aprovado");
+}else {
+   console.log("Reprovado");
+}
+
+if (aprovado == 1) {
+   console.log("Aprovado");
+}
+
+if ('0' == 0) {
+   console.log("Iguais");
+}else {
+   console.log("Diferentes");
+}
+
+if ('0' === 0) {
+   console.log("Iguais");   
+}else {
+   console.log("Diferentes");     
+ }
