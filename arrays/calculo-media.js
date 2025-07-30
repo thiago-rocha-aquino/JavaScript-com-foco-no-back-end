178cad419d64b9e41a38a171aabdfbe1a5d566ca
Thiago Rocha de Aquino <thiagorocha948@uni9.edu.br>
2025-07-29 21:19:24 -0300

    feat(main): trabalhando com valores de um array, utilizando estrutura de repetição e algumas funções(push, pop)

--- arrays/calculo-media.js
+++ arrays/calculo-media.js
@@ -1,7 +1,7 @@
 const notas = [10, 6.5, 8, 7.5];
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) /notas.length;//lengeth é o tamanho do array, ou seja, a quantidade de elementos que ele possui. 
                                                                         //length é uma propriedade do array que retorna o número de elementos contidos nele.
-console.log(`A média é: ${media}`);
+console.log(`A média é: ${media} \n`);
 
 
 const calculo = () => {
@@ -19,4 +19,6 @@ mediaa /= notasTotal.length;
 return mediaa;
  
 }
-console.log(`A média é: ${calculo()}`);
+console.log(`A média é: ${calculo().toFixed(1)}\n`);
+
+
