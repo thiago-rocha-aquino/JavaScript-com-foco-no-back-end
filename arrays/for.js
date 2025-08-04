821962b601b9f0195fb0145faf51f218f6bb1478
Thiago Rocha de Aquino <thiagorocha948@uni9.edu.br>
2025-08-04 19:43:08 -0300

    feat(main): estrutura básica de for(estrutura de repetição)

---
+++ arrays/for.js
@@ -0,0 +1,6 @@
+const notas = [10, 8.5, 5, 6.5, 8, 7.5];
+
+for (i = 0; i < notas.length; i++) {
+    console.log(notas.length) // length é o tamanho do array, ou seja, a quantidade de elementos que ele possui.
+    console.log(`Nota ${i + 1}: ${notas[i]}`);
+}
