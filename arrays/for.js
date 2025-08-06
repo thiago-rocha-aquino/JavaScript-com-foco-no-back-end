2226e4cc2b45e19fb5d9aaf1a0cc0cff2bdb674a
Thiago Rocha de Aquino <thiagorocha948@uni9.edu.br>
2025-08-06 18:34:05 -0300

    feat(main): update

--- arrays/for.js
+++ arrays/for.js
@@ -4,3 +4,18 @@ for (i = 0; i < notas.length; i++) {
     console.log(notas.length) // length é o tamanho do array, ou seja, a quantidade de elementos que ele possui.
     console.log(`Nota ${i + 1}: ${notas[i]}`);
 }
+
+let somaDasNotas = 0;
+for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i]; // somaDasNotas = somaDasNotas + notas[i];
+}
+const media = somaDasNotas / notas.length;
+console.log(`A média das notas é: ${media}`);
+
+
+
+const numeros = [5, 10, 15, 20, 25];
+
+for (let i = numeros.length - 1; i>=0; i--){
+    console.log(`numero ${i +1}: ${numeros[i]}`);
+}
